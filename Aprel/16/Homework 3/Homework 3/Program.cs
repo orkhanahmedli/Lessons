68e70851e3cb5ee74988ae5d7d858e2c0cf63fa1
Orxan Axmedli <valleywolfes@gmail.com>
2023-04-17 05:52:36 +0400

    gonder bunuda hazirdi

---
+++ Aprel/16/Homework 3/Homework 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Sheki";
+            var b = "Kelbecer";
+            var c = "Xankendi";
+            var d = "Fuzuli";
+            var f = "Lachin";
+            var e = "Shusha";
+            var g = "Aghdam";
+            var h = "Zengilan";
+            var r = "Qubadli";
+            var t = "Cebrayil";
+
+            Console.Write("Sheki");
+            Console.Write("," + "Kelbecer");
+            Console.Write("," + "Xankendi");
+            Console.Write("," + "Fuzuli");
+            Console.Write("," + "Lachin");
+            Console.Write("," + "Shusha");
+            Console.Write("," + "Aghdam");
+            Console.Write("," + "Zengilan");
+            Console.Write("," + "Qubadli");
+            Console.Write("," + "Cebrayil");
+            Console.ReadLine();
+        }
+    }
+}
